b16b63c3991a2b4d75cfebd24010c22ce310e9e7
Rest <code.rest.dev@gmail.com>
2024-02-09 19:44:24 -0300

    0.1.2 Actualizar tareas - Update Task

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -5,5 +5,8 @@ const tasksApi = axios.create({
 });
 
 export const getAllTasks = () => tasksApi.get("/");
+export const getTask = (id) => tasksApi.get("/" + id + "/" )
 export const createTask = (task) => tasksApi.post("/", task);
 export const deleteTask = (id) => tasksApi.delete("/" + id)
+// export const updateTask = (id , task) => tasksApi.put(`/${id}/`, task)
+export const updateTask = (id , task) => tasksApi.put("/" + id + "/" , task)
