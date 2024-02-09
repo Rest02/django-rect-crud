48ae2a4d588b4be8394973eaa1682f5142c43318
Rest <code.rest.dev@gmail.com>
2024-02-08 21:01:14 -0300

    0.1.0 Crear Tareas - PUT

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import { createTask } from "../api/tasks.api";
+import { useNavigate } from 'react-router-dom'
+
 export function TaskFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate  = useNavigate()
+
+  const enviar = handleSubmit(async (data) => {
+    const res = await createTask(data);
+    console.log(res);
+    navigate("/tasks")
+  });
+
   return (
-    <div>TaskFormPage</div>
-  )
+    <div>
+      <form onSubmit={enviar}>
+        <input
+          type="text"
+          placeholder="title"
+          {...register("title", { required: true })}
+        />
+        {errors.title && <span>title is required</span>}
+
+        <textarea
+          name=""
+          rows="3"
+          placeholder="Description"
+          {...register("description", { required: true })}
+        ></textarea>
+        {errors.description && <span>description is required</span>}
+
+        <button>Save</button>
+      </form>
+    </div>
+  );
 }
 
-export default TaskFormPage
+export default TaskFormPage;
